ce9b9b2b9874fd1557bdfb7a0c9ed2dbe725454d
Ben Demboski <bend@meevine.com>
2012-09-22 18:06:44 -0700

    Base HTML template and placeholder landing page.

--- lvfu/urls.py
+++ lvfu/urls.py
@@ -1,17 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
+from django.views.generic import RedirectView, TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lvfu.views.home', name='home'),
-    # url(r'^lvfu/', include('lvfu.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name='home.html')),
+    (r'^admin$', RedirectView.as_view(url='/admin/', permanent=True)),
+    (r'^admin/', include(admin.site.urls)),
 )
