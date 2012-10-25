0ba7091a7e6dd742cf03b7d7686a2a553f2ec9d6
Ben Demboski <bend@meevine.com>
2012-10-25 01:38:00 -0700

    Fix and tune routine for pulling scores from leaguevine

--- lvfu/fantasy/utils.py
+++ lvfu/fantasy/utils.py
@@ -5,19 +5,18 @@ from models import Player
 from rules import STATS_FIELDS, compute_score
 
 
-def update_player_scores(league, players=None):
-    if players is None:
-        players = Player.objects.get_for_league(league)
-    if not players:
+def update_player_scores(league):
+    all_players = Player.objects.get_for_league(league)
+    players = all_players.distinct('lv_player_id')
+    # Use len because if it's non-zero we're going to load all the rows anyway
+    if len(players) == 0:
         return
 
-    player_map = dict([(p.lv_player_id, p) for p in players])
     results = get_player_stats([p.lv_player_id for p in players],
                                league.event.type,
                                league.event.lv_id,
                                STATS_FIELDS)
     for player_id, stats in results.iteritems():
-        player = player_map[player_id]
-        player.score = compute_score(stats)
-        player.score_updated = datetime.utcnow()
-        player.save()
+        (all_players.filter(lv_player_id=player_id)
+                    .update(score=compute_score(stats),
+                            score_updated = datetime.utcnow()))
